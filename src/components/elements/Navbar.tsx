5c2b1a047856993bed8a0c8e8cb23225131927d5
c donny <c.donny0000@gmai.com>
2025-05-26 16:14:31 +0700

    Fixed: Navbar gap

--- src/components/elements/Navbar.tsx
+++ src/components/elements/Navbar.tsx
@@ -27,7 +27,7 @@ export const Navbar = () => {
                         </a>
                     </div>
     
-                    <div className="flex flex-col lg:flex-row w-full lg:justify-between lg:items-center absolute top-full left-0 lg:static lg:top-0 bg-body lg:bg-transparent border-x border-x-box-border lg:border-x-0 lg:h-auto h-0 overflow-hidden">
+                    <div className="flex flex-col lg:flex-row w-full lg:justify-between lg:items-center absolute top-full left-0 lg:static lg:top-0 bg-body lg:bg-transparent border-x border-x-box-border lg:border-x-0 lg:h-auto h-0 overflow-hidden gap-4">
                             <ul className="border-t border-box-border lg:border-t-0 px-6 lg:px-0 pt-6 lg:pt-0 flex flex-col lg:flex-row gap-y-4 gap-x-3 text-lg text-heading-2 w-full lg:justify-center lg:items-center"> 
                             {navItems.map((item, key) => (
                                 <li key={key}>
